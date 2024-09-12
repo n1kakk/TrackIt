7bedc0fe184358a9a9fcfd8b19e9efb1e0932a8b
N1kakk <veronikakolesova2003@gmail.com>
2024-09-12 19:09:46 +0300

    Added - IParcelRepository,ParcelRepository(not implemented)

---
+++ src/Services/ParcelService/Repositories/IParcelRepository.cs
@@ -0,0 +1,13 @@
+﻿using ParcelService.Entities;
+
+namespace ParcelService.Repositories;
+
+public interface IParcelRepository
+{
+    void AddParcel(Parcel parcel);
+    Task<bool> SaveChangesAsync();
+    Task<List<Parcel>> GetNewParcels();
+    Task<List<Parcel>> GetNewParcelsInCountry(string country);
+    Task<List<Parcel>> GetNewParcelsFromCity(string city);
+    Task<bool> UodateStatus(Status status);
+}
---
+++ src/Services/ParcelService/Repositories/ParcelRepository.cs
@@ -0,0 +1,36 @@
+﻿using ParcelService.Entities;
+
+namespace ParcelService.Repositories;
+
+public class ParcelRepository : IParcelRepository
+{
+    public void AddParcel(Parcel parcel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Parcel>> GetNewParcels()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Parcel>> GetNewParcelsFromCity(string city)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Parcel>> GetNewParcelsInCountry(string country)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveChangesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UodateStatus(Status status)
+    {
+        throw new NotImplementedException();
+    }
+}
